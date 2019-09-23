c279bb50498515c9f59400cd8d77e41a8f21948e
CrisRod <crodriguez85@gmail.com>
2019-09-23 11:20:58 -0300

    server basics

---
+++ server/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server Started on port: ${port}`);
+})
