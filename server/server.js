c29b8a25c57bc9a4a4db483eb8352e833ee3efe7
CrisRod <crodriguez85@gmail.com>
2019-09-23 11:31:00 -0300

    mongo connect

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
 require('dotenv').config();
 
 const app = express();
@@ -8,6 +9,18 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+    useNewUrlParser: true, 
+    useCreateIndex: true, 
+    useUnifiedTopology: true
+});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established succesfully");
+});
+
 app.listen(port, () => {
-    console.log(`Server Started on port: ${port}`);
+    console.log(`Server Started on localhost:${port}`);
 })
